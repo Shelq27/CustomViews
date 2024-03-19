e7d9a7010f97f653d1021764af521bffefb372f2
Shelq <klcao@yandex.ru>
2024-03-19 23:40:51 +0300

    DZ 1-2-3

---
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -0,0 +1,116 @@
+package ru.netology.statsview.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.withStyledAttributes
+import ru.netology.statsview.R
+import ru.netology.statsview.utils.AndroidUtils
+import kotlin.math.min
+import kotlin.random.Random
+
+
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+
+    ) : View(
+    context,
+    attributeSet,
+    defStyleAttr,
+    defStyleRes
+) {
+    private var textSize = AndroidUtils.dp(context, 20).toFloat()
+    private var lineWith = AndroidUtils.dp(context, 5)
+    private var colors = emptyList<Int>()
+
+    init {
+        context.withStyledAttributes(attributeSet, R.styleable.StatsView) {
+            textSize = getDimension(R.styleable.StatsView_textSize, textSize)
+            lineWith = getDimension(R.styleable.StatsView_lineWidth, lineWith.toFloat()).toInt()
+
+            colors = listOf(
+                getColor(R.styleable.StatsView_color1, generateRandomColor()),
+                getColor(R.styleable.StatsView_color2, generateRandomColor()),
+                getColor(R.styleable.StatsView_color3, generateRandomColor()),
+                getColor(R.styleable.StatsView_color4, generateRandomColor())
+            )
+        }
+    }
+
+    var data: List<Float> = emptyList()
+        set(value) {
+            field = value
+            invalidate()
+        }
+    private var radius = 0F
+    private var center = PointF()
+    private var oval = RectF()
+    private var paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeWidth = lineWith.toFloat()
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+    private var textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = this@StatsView.textSize
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+    }
+
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - lineWith
+        center = PointF(w / 2F, h / 2F)
+        oval = RectF(
+            center.x - radius,
+            center.y - radius,
+            center.x + radius,
+            center.y + radius,
+        )
+
+    }
+
+    override fun onDraw(canvas: Canvas) {
+
+        paint.color = 0xFFD3D3D3.toInt()
+        canvas.drawCircle(center.x, center.y, radius, paint)
+
+        if (data.isEmpty()) {
+            canvas.drawText(
+                "%.2f%%".format(data.sum() * 100),
+                center.x,
+                center.y + textPaint.textSize / 4,
+                textPaint
+            )
+            return
+        }
+        var startAngle = -90F
+        data.forEachIndexed { index, datum ->
+            val angle = datum * 0.18F
+            paint.color = colors.getOrElse(index) { generateRandomColor() }
+            canvas.drawArc(oval, startAngle, angle, false, paint)
+            startAngle += angle
+        }
+
+        startAngle=-90F
+        paint.color = (colors[0])
+        canvas.drawArc(oval, startAngle, data[0] * 0.18F, false, paint)
+
+        canvas.drawText(
+            "%.2f%%".format(data.sum() * 0.05F),
+            center.x,
+            center.y + textPaint.textSize / 4,
+            textPaint
+        )
+
+    }
+
+    private fun generateRandomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+}
